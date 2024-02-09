eb2028473ee4f0bd5af945e4ad3606a9316b0ea6
Ushitora Anqou <ushitora@anqou.net>
2024-02-09 23:18:43 +0900

    wip

--- e2e/e2e.ml
+++ e2e/e2e.ml
@@ -1,16 +1,18 @@
 exception Process_status_error of Unix.process_status * string * string
 
-let eventually ?(count = 3) ?(interval = 1) f =
+let or_true f = try f () with Process_status_error _ -> ()
+let failwithf f = Printf.ksprintf failwith f
+
+let eventually ?(count = 60) ?(interval = 5) f =
   let rec loop i =
     try f () with
     | e when i = 0 -> raise e
-    | _ ->
+    | e ->
+        Logs.err (fun m -> m "eventually: failed: %s" (Printexc.to_string e));
         Unix.sleep interval;
         loop (i - 1)
   in
   loop count
-
-[@@@warning "-21"]
 
 let run_command command =
   let stdout_ch, stdin_ch, stderr_ch =
@@ -25,35 +27,72 @@ let run_command command =
   in
   let stdout = In_channel.input_all stdout_ch in
   let stderr = In_channel.input_all stderr_ch in
+  Logs.info (fun m ->
+      m "RUN: %s\nstdout: %s\nstderr: %s" (String.trim command)
+        (String.trim stdout) (String.trim stderr));
   match Unix.close_process_full (stdout_ch, stdin_ch, stderr_ch) with
   | WEXITED 0 -> (stdout, stderr)
   | status -> raise (Process_status_error (status, stdout, stderr))
 
-let wait_deploy_available name =
+let kubectl args =
+  let kubectl = Sys.getenv_opt "KUBECTL" |> Option.value ~default:"kubectl" in
+  run_command (kubectl ^ " " ^ args)
+
+let wait_available kind condition name =
   try
-    run_command
-      ({|kubectl wait --for='condition=Available' --timeout=60s deploy |} ^ name)
+    kubectl
+      (Printf.sprintf {|wait --for='condition=%s' --timeout=1s %s %s|} condition
+         kind name)
     |> ignore
   with Process_status_error (Unix.WEXITED 1, _, _) ->
-    failwith ("condition Available not met for deployments/" ^ name)
+    failwith ("condition Available not met for " ^ kind ^ "/" ^ name)
+
+let wait_deploy_available name = wait_available "deploy" "Available" name
+let wait_pod_available name = wait_available "pod" "Ready" name
 
 let http_get uri =
-  run_command
-    (Printf.sprintf {|kubectl exec -it deploy/toolbox -- curl --silent '%s'|}
-       uri)
+  kubectl (Printf.sprintf {|exec deploy/toolbox -- curl --silent '%s'|} uri)
 
 let apply_manifest file_name =
-  run_command (Printf.sprintf {|kubectl apply -f manifests/%s|} file_name)
-  |> ignore
+  kubectl (Printf.sprintf {|apply -f manifests/%s|} file_name) |> ignore
 
 let delete_manifest file_name =
-  run_command (Printf.sprintf {|kubectl apply -f manifests/%s|} file_name)
+  kubectl (Printf.sprintf {|delete -f manifests/%s|} file_name) |> ignore
+
+let check_mastodon_version ~host ~endpoint ~expected =
+  let got =
+    kubectl
+      (Printf.sprintf
+         {|exec deploy/toolbox -- curl --silent -H 'Host: %s' %s/api/v1/instance | jq -r .version|}
+         host endpoint)
+    |> fst |> String.trim
+  in
+  if got <> expected then
+    failwithf "check_mastodon_version: got %s, expected %s" got expected
+
+let wait_not_found kind name =
+  kubectl
+    (Printf.sprintf {|get %s %s 2>&1 | grep "\"%s\" not found"|} kind name name)
   |> ignore
 
-let () =
-  wait_deploy_available "mastodon-operator";
+let setup () =
+  or_true (fun () -> kubectl {|delete deploy mastodon-operator|} |> ignore);
+  or_true (fun () -> kubectl {|delete mastodon mastodon0|} |> ignore);
+  or_true (fun () -> delete_manifest "postgres.yaml" |> ignore);
+  or_true (fun () -> delete_manifest "redis.yaml" |> ignore);
+  or_true (fun () -> apply_manifest "postgres.yaml");
+  or_true (fun () -> apply_manifest "redis.yaml");
+  or_true (fun () -> apply_manifest "operator.yaml");
+  eventually (fun () -> wait_pod_available "postgres-0");
+  eventually (fun () -> wait_pod_available "redis-0");
+  eventually (fun () -> wait_deploy_available "mastodon-operator");
+  ()
 
-  apply_manifest "mastodon0.yaml";
+let () =
+  Mahout.Logg.setup ();
+  setup ();
+
+  apply_manifest "mastodon0-v4.1.9.yaml";
 
   eventually (fun () ->
       wait_deploy_available "mastodon0-gateway-nginx";
@@ -61,8 +100,27 @@ let () =
       wait_deploy_available "mastodon0-streaming";
       wait_deploy_available "mastodon0-web";
       http_get "http://mastodon0-gateway-svc.default.svc/health" |> ignore;
+      check_mastodon_version ~host:"mastodon0.ket-apps.test"
+        ~endpoint:"http://mastodon0-gateway-svc.default.svc" ~expected:"4.1.9";
       ());
 
-  delete_manifest "mastodon0.yaml";
+  apply_manifest "mastodon0-v4.2.0.yaml";
+
+  eventually (fun () ->
+      wait_deploy_available "mastodon0-gateway-nginx";
+      wait_deploy_available "mastodon0-sidekiq";
+      wait_deploy_available "mastodon0-streaming";
+      wait_deploy_available "mastodon0-web";
+      http_get "http://mastodon0-gateway-svc.default.svc/health" |> ignore;
+      check_mastodon_version ~host:"mastodon0.ket-apps.test"
+        ~endpoint:"http://mastodon0-gateway-svc.default.svc" ~expected:"4.2.0";
+      ());
+
+  delete_manifest "mastodon0-v4.2.0.yaml";
+  eventually (fun () ->
+      wait_not_found "deploy" "mastodon0-gateway-nginx";
+      wait_not_found "deploy" "mastodon0-sidekiq";
+      wait_not_found "deploy" "mastodon0-streaming";
+      wait_not_found "deploy" "mastodon0-web");
 
   ()
